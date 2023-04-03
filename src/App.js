532776effef209024a3f9c18100a6a245ce7c1c1
wiredbrain55 <dinosaurlearning@gmail.com>
2023-04-02 22:00:20 -0400

    setup streetview stuff fix env later

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import './App.css';
-import Map from './components/Map'
-
+import Map from './components/Map';
+import { GoogleMap, LoadScript, StreetViewPanorama } from '@react-google-maps/api';
 // TODO make this a component
+
+const center = {
+  lat: 42.331891493532964, 
+  lng: -71.10145404754537
+};
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+
 const App = () => {
 
   return (
+    <>
+    <LoadScript 
+      // fix env stuff later idk why its broken as fuck
+      googleMapsApiKey="" // secret key plz add
+    >
+      <GoogleMap
+    id="streetview"
+    mapContainerStyle={containerStyle}
+    zoom={7}
+    center={center}
+  >
+    <StreetViewPanorama
+      position={center}
+      visible={true}
+    />
+  </GoogleMap>
+    </LoadScript>
     <div className = "map">
       <Map />
     </div>
+    </>
   )
 }
 
